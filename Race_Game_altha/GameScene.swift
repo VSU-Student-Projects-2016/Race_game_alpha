d5c16751905b664399202fa49ccc0d80ec1d4401
Fyodor <www.realmanfyodor@mail.ru>
2016-10-22 13:01:20 +0300

    Update 22.10.2016

--- Race_Game_altha/GameScene.swift
+++ Race_Game_altha/GameScene.swift
@@ -23,13 +23,22 @@ struct PhysicsCategory{
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
+    var Score = Int()
+    
     var Player = SKSpriteNode(imageNamed: "Car;).png")
+    
+    var ScoreLbl = UILabel()
+    
+    
+    
     
     var timer, timerE: Timer!
     
     override func didMove(to view: SKView) {
         
-        Player.position = CGPoint(x:0, y: -self.size.height / 5)
+        physicsWorld.contactDelegate = self
+        
+        Player.position = CGPoint(x:0, y: -self.size.height / 10)
         
         Player.physicsBody = SKPhysicsBody(rectangleOf: Player.size)
         Player.physicsBody?.affectedByGravity = false;
@@ -48,21 +57,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         RunLoop.current.add(timerE, forMode: .commonModes)
         
         self.addChild(Player)
+        
+        ScoreLbl.text = "\(Score)"
+        //ScoreLbl.frame =
+        //ScoreLbl = UILabel(frame: CGRect(x: 200, y: 200, width: 100, height: 20))
+        //ScoreLbl.backgroundColor = UIColor(
+        
     }
+    
+    
     
     func didBegin(_ contact: SKPhysicsContact) {
-        <#code#>
+        var firstBody:SKPhysicsBody = contact.bodyA
+        var secondBody:SKPhysicsBody = contact.bodyB
+        
+        if ((firstBody.categoryBitMask == PhysicsCategory.Enemy) && (secondBody.categoryBitMask == PhysicsCategory.Bullet) ||
+            (firstBody.categoryBitMask == PhysicsCategory.Bullet) && (secondBody.categoryBitMask == PhysicsCategory.Enemy)){
+            
+            //if botj bodies is SkSprite
+            // if let a = firstBody.node as? SKSpriteNode, 
+            //let b = se {
+              //  Collisiont(a, b)
+            //}
+            CollisionWithBullet(Enemy: firstBody.node as! SKSpriteNode, Bullet: secondBody.node as! SKSpriteNode)
+        }
     }
     
-    
-    
-    
+    func CollisionWithBullet(Enemy : SKSpriteNode, Bullet : SKSpriteNode){
+        Enemy.removeFromParent()
+        Bullet.removeFromParent()
+        
+        Score+=1
+        
+        NSLog("\(Score)")
+        
+    }
     
     func SpawnBullets(){
         let Bullet = SKSpriteNode(imageNamed: "ball.png")
         Bullet.zPosition = -5
         Bullet.position = CGPoint(x:Player.position.x, y: Player.position.y)
-        let action = SKAction.moveTo(y: self.size.height + 32, duration: 1.2)
+        let action = SKAction.moveTo(y: self.size.height/10 + 32, duration: 1.2)
         let actionDone = SKAction.removeFromParent()
         
         Bullet.run(SKAction.sequence([action, actionDone]))
@@ -81,20 +116,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func SpawnEnemies(){
     
         var Enemy = SKSpriteNode(imageNamed: "Car;).png")
-        var MinValue = self.size.width / 8
-        var MaxValue = self.size.width - 80
+        var MinValue = self.size.width / 10 - 200
+        var MaxValue = self.size.width - 300
         var SpawnPoint = UInt32(MaxValue - MinValue)
         
         Enemy.position = CGPoint(x: CGFloat(arc4random_uniform(SpawnPoint)),
-            y:self.size.height / 2.0)
+            y:self.size.height / 10)
         print(Enemy.position)
-        let action = SKAction.moveTo(y: -size.height/2.0 - 70, duration: 1.2)
+        let action = SKAction.moveTo(y: -size.height/10 - 100, duration: 1.2)
         
         let actionDone = SKAction.removeFromParent()
         
         
         
         Enemy.run(SKAction.sequence([action, actionDone]))
+        
+        
         
         Enemy.physicsBody = SKPhysicsBody(rectangleOf: Enemy.size)
         Enemy.physicsBody?.affectedByGravity = false;
